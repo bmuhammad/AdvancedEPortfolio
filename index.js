1c97492ed6e858d6a2264a99cf4b294893666a26
Brian <briancmuhammad@yahoo.com>
2024-07-12 17:45:21 -0400

    finished modal, started constrasting toggle

---
+++ index.js
@@ -0,0 +1,39 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    constrastToggle = !contrastToggle
+    if(contrastToggle) {
+        document.body.classList += " dark-theme" 
+    }
+else{
+    document.body.classList.remove("dark-theme");
+}
+
+}
+
+function contact(event) {
+  event.preventDefault();
+
+  loading = document.querySelector(".modal__overlay--loading");
+  success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  setTimeout(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    console.log("it worked");
+  }, 1000);
+}
+
+function toggleModal() {
+ 
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+       
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
