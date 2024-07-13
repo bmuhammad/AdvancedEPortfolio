e70438264d183379c3157021950093030d5e4064
Brian <briancmuhammad@yahoo.com>
2024-07-13 18:03:33 -0400

    added responsive code. finished site. would still need to add email functionality

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
-function toggleContrast() {
-    contrastToggle = !contrastToggle
-    if(contrastToggle) {
-        document.body.classList += " dark-theme" 
+function moveBackground(event) {
+ 
+
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      // Added rotate after tutorial
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
     }
-else{
-    document.body.classList.remove("dark-theme");
 }
 
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
 function contact(event) {
@@ -27,13 +41,10 @@ function contact(event) {
 }
 
 function toggleModal() {
- 
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open")
-       
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
-
